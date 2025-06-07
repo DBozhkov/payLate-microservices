bc7ca08fcb8147c47dce7c0717c4f2eaf6a84844
Dimitar Bozhkov <dimitarbzhkov@gmail.com>
2025-06-07 17:54:38 +0300

    Added new okta auth

--- frontend/react-paylateapp/src/lib/oktaConfig.ts
+++ frontend/react-paylateapp/src/lib/oktaConfig.ts
@@ -1,5 +1,5 @@
 export const oktaConfig = {
-    clientId: '0oalb9wiz4IsSAhdu5d7',
+    clientId: '0oap2yme859gAvIWZ5d7',
     issuer: 'https://dev-73369143.okta.com/oauth2/default',
     redirectUri: window.location.origin + '/login/callback',
     scopes: ['openid', 'profile', 'email', 'manage', 'read'],
